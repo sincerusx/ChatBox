aebe1f665e7bbf6a4cc42f0a67b45e4dea014ad5
Liam Nelson <lmjnelson@yahoo.com>
2015-10-26 13:23:38 +0000

    Brought back Whoops from Laravel 4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,1 @@
-Home
+@extends('templates.default')@section('content')    <h3>Welcome to ChatBox</h3>    <p>The best social network...ever!</p>@stop
--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -4,10 +4,16 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-	<link rel="stylesheet" href="">
+	<title>ChatBox</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 <body>
-	
+
+    @include('templates/navigation')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
 </body>
 </html>
